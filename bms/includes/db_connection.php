e4514c7218fff35597c6ec8f7ac49d229c2e8f9e
Pawan Perera <pawanperera5@gmail.com>
2026-06-15 15:05:04 +0530

    feat: implement database connection script with environment-specific configuration

--- bms/includes/db_connection.php
+++ bms/includes/db_connection.php
@@ -1,28 +1,22 @@
 <?php
-// require_once __DIR__ . '/../config/paths.php';
-
-// date_default_timezone_set('Asia/Colombo');
-// // Database connection
-// $servername = 'localhost';
-// $username   = 'root';
-// $password   = '';
-// $dbname     = 'bms';
-
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
 require_once __DIR__ . '/../config/paths.php';
 
 date_default_timezone_set('Asia/Colombo');
-// Database connection
-$servername = 'localhost';
-$username   = 'eaglete1_admin';
-$password   = 'eaglet@21';
-$dbname     = 'eaglete1_bms';
+
+// Detect environment
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+    // Local Database
+    $servername = 'localhost';
+    $username   = 'root';
+    $password   = '';
+    $dbname     = 'bms';
+} else {
+    // Live Database
+    $servername = 'localhost';
+    $username   = 'eaglete1_admin';
+    $password   = 'eaglet@21';
+    $dbname     = 'eaglete1_bms';
+}
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
