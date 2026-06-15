3cd82e96829c30da2024de72f420e940b2906f8e
Pawan Perera <pawanperera5@gmail.com>
2026-06-15 09:19:49 +0530

    feat(db): update database connection credentials for production environment

--- bms/includes/db_connection.php
+++ bms/includes/db_connection.php
@@ -1,12 +1,28 @@
 <?php
+// require_once __DIR__ . '/../config/paths.php';
+
+// date_default_timezone_set('Asia/Colombo');
+// // Database connection
+// $servername = 'localhost';
+// $username   = 'root';
+// $password   = '';
+// $dbname     = 'bms';
+
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
 require_once __DIR__ . '/../config/paths.php';
 
 date_default_timezone_set('Asia/Colombo');
 // Database connection
 $servername = 'localhost';
-$username   = 'root';
-$password   = '';
-$dbname     = 'bms';
+$username   = 'eaglete1_admin';
+$password   = 'eaglet@21';
+$dbname     = 'eaglete1_bms';
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
